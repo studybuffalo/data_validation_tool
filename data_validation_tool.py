33e75c9e03b6da8be8175c520e89c00f58de527d
studybuffalo <studybuffalo@gmail.com>
2017-03-31 20:12:44 -0600

    Initial build of GUI

--- data_validation_tool.py
+++ data_validation_tool.py
@@ -24,13 +24,61 @@
     SHOULD YOU REQUIRE ANY EXCEPTIONS TO THIS LICENSE, PLEASE CONTACT 
     THE COPYRIGHT HOLDERS.
 """
+def create_menu_bar(root):
+    root.option_add("*tearOff", FALSE)
+
+    menubar = Menu(root)
+
+    menuFile = Menu(menubar)
+    menuFile.add_command(label="Close", command=root.quit)
+    menuAbout = Menu(menubar)
+    menubar.add_cascade(menu=menuFile, label="File")
+    menubar.add_cascade(menu=menuAbout, label="About")
+
+    root.config(menu=menubar)
+
+def abc_validation(root, dashboard):
+    log.debug("Creating the validation window")
+    
+    # Remove old content
+    dashboard.destroy()
+    root.title("Study Buffalo Data Validation Tool - ABC DBL Validation")
+
+    abc = ttk.Frame(root, padding="3 3 12 12")
+
+def close_program():
+    log.debug("Closing program")
+
+def create_dashboard(root):
+    root.title("Study Buffalo Data Validation Tool")
+
+    dashboard = ttk.Frame(root, padding="3 3 12 12")
+    dashboard.grid(column=0, row=0, sticky=(N, W, E, S), padx=5, pady=5)
+    dashboard.columnconfigure(0, weight=1)
+    dashboard.columnconfigure(0, weight=1)
+
+    titleFont = font.Font(size=16, weight="bold")
+    appTitle = ttk.Label(dashboard, text="Study Buffalo Data Validation Tool", font=titleFont)
+    appTitle.grid(column=0, row=0, sticky=(N, W), pady=(0,10))
+
+    subFont = font.Font(size=13, underline=True)
+    appTitle = ttk.Label(dashboard, text="Choose Data Source:", font=subFont)
+    appTitle.grid(column=0, row=1, sticky=(N, W))
+
+    abcButton = ttk.Button(dashboard, 
+                           text="ABC iDBL Data Extraction Tool",
+                           command=lambda: abc_validation(root, dashboard))
+    abcButton.grid(column=0, row=2, sticky=(N, W), pady=5, padx=(15, 0))
+
+    hcButton = ttk.Button(dashboard, text="HC DPD Data Extraction Tool")
+    hcButton.grid(column=0, row=3, sticky=(N, W), pady=5, padx=(15, 0))
 
 import sys
 from unipath import Path
 import configparser
 import python_logging
-import tkinter
-from tkinter import ttk
+from tkinter import *
+from tkinter import ttk, font
 
 # APPLICATION SETUP
 # Set root path to allow retrieval of other files
@@ -38,14 +86,25 @@ root = Path(sys.argv[1])
 
 # Setup the public config file
 pubCon = configparser.ConfigParser()
-pubCon.read(root.child("abc_config.cfg").absolute())
+pubCon.read(root.child("data_validation_config.cfg").absolute())
 
 # Setup the private config file
 priCon = configparser.ConfigParser()
 priCon.read(Path(pubCon.get("misc", "private_config")).absolute())
 
 # Setup the logging object
-log = python_logging(priCon)
+log = python_logging.start(priCon)
 
-root = tkinter.Tk()
-root.title("Study Buffalo Data Validation Tool")
+# Setting up the program window
+root = Tk()
+
+# Set to maximize
+root.wm_state('zoomed')
+
+# Creating Menu Bar
+create_menu_bar(root)
+
+# Start initial program window
+create_dashboard(root)
+
+root.mainloop()
